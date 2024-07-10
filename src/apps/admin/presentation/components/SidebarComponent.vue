b872b894b0ac9c2ceabdf995d72595fce02414ef
Abreham Atlaw <abreham.atlaw@yahoo.com>
2024-07-10 18:06:44 +0300

    Import Navigation

--- src/apps/admin/presentation/components/SidebarComponent.vue
+++ src/apps/admin/presentation/components/SidebarComponent.vue
@@ -99,6 +99,15 @@ let isOpen = ref(true);
 
           <span class="mx-4">Report</span>
         </router-link>
+        <router-link
+          class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
+          :class="[$route.name === 'Cards' ? activeClass : inactiveClass]"
+          to="/admin/import"
+        >
+          <i class="fa-solid fa-file-import"></i>
+
+            <span class="mx-4">Import</span>
+        </router-link>
 
       </nav>
       <nav class="mt-24">
